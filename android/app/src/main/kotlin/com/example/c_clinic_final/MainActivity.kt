6b84317df57a80f2a4ae6fabedb8b6fc393832cc
Yujeong Lee <uj1321@naver.com>
2023-07-11 01:04:11 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/c_clinic_final/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.c_clinic_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:c_clinic_final/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'auth_service.dart';
+import 'login_page.dart';
+
+const clinic_color = Color.fromARGB(255, 41, 75, 43);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // main 함수에서 async 사용하기 위함
+  await Firebase.initializeApp(); // firebase 앱 시작
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final user = context.read<AuthService>().currentUser();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: user == null ? LoginPage() : HomePage(),
+    );
+  }
+}
